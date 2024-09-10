7c08012738ad050078edcbe47caf10a8eeae65d8
TatePadilla <tate.padilla123@gmail.com>
2024-09-09 22:20:00 -0500

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        // EF Core will configure the database to generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")] 
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")] 
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")] 
+        public int? Rating { get; set; }
+
+        // Tying in the Genre entity.
+        [Required(ErrorMessage = "Please enter a genre.")] 
+        // string.Empty indicates that this is a foreign key field/property(primary key within Genre class).
+        public string GenreId { get; set; } = string.Empty; [ValidateNever]
+        // Relating the Genre entity to the Movie by coding a property with that entity class as a datatype.
+        public Genre Genre { get; set; } = null!;
+
+        // Allowing for the optional slug property.
+        public string Slug =>
+        Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+namespace Ch04MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        { }
+        public DbSet<Movie> Movies { get; set; } = null!;
+        
+        // Any new entities (Genres) must be added to the DbContext class as DbSet properties.
+        public DbSet<Genre> Genres { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Setting intial seed data (NOT REQUIRED).
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre { GenreId = "A", Name = "Action" }, 
+                new Genre { GenreId = "C", Name = "Comedy" }, 
+                new Genre { GenreId = "D", Name = "Drama" }, 
+                new Genre { GenreId = "H", Name = "Horror" }, 
+                new Genre { GenreId = "M", Name = "Musical" }, 
+                new Genre { GenreId = "R", Name = "RomCom" }, 
+                new Genre { GenreId = "S", Name = "SciFi" }
+            ); 
+            
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    // Setting intial seed data (NOT REQUIRED).
+                    MovieId = 1,
+                    Name = "Casablanca",
+                    Year = 1942,
+                    Rating = 5,
+                    GenreId = "D"
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3,
+                    GenreId = "A"
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4,
+                    GenreId = "R"
+                }
+            );
+        }
+    }
+}
